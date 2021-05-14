1dda9ff46d258a6d48071561d0f5e003430234d2
“ordonezvalina” <ordonez.valina@gmail.com>
2021-05-15 01:47:25 +0200

    first approach to xml and json implementation

--- src/main/java/com/example/rindus/ConsumingRestApplication.java
+++ src/main/java/com/example/rindus/ConsumingRestApplication.java
@@ -1,6 +1,7 @@
 package com.example.rindus;
 
-
+import org.json.JSONObject;
+import org.json.XML;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -10,12 +11,23 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
 @SpringBootApplication
 public class ConsumingRestApplication {
 
     private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
 
     public static void main(String[] args) {
+        System.out.println("Amigo de Vigo de Bigote");
         SpringApplication.run(ConsumingRestApplication.class, args);
     }
 
@@ -27,9 +39,22 @@ public class ConsumingRestApplication {
     @Bean
     public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
         return args -> {
-            Quote quote = restTemplate.getForObject(
-                    "https://quoters.apps.pcfone.io/api/random", Quote.class);
-            log.info(quote.toString());
+            Todo todo = restTemplate.getForObject(
+                    "https://jsonplaceholder.typicode.com/todos/2", Todo.class);
+            log.info(todo.toString());
+
+            ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+            String json = ow.writeValueAsString(todo);
+            log.info(json.toString());
+
+            JSONObject jsonObject = new JSONObject(json);
+            String xml = XML.toString(jsonObject);
+            log.info(xml);
+
+            try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream("filename.xml"), "utf-8"))) {
+                writer.write(xml);
+            }
         };
     }
 }
---
+++ src/main/java/com/example/rindus/Todo.java
@@ -0,0 +1,57 @@
+package com.example.rindus;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Todo {
+
+    private int userId;
+    private int id;
+    private String title;
+    private Boolean completed;
+
+    public Todo() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "userId=" + userId +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                ", completed='" + completed + '\'' +
+                '}';
+    }
+}
